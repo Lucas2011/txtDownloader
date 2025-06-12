b2e8b64914dabcf7fd81d25da5ff4239212a330b
Lucas2011 <31942492+Lucas2011@users.noreply.github.com>
2025-06-12 13:42:12 -0700

    Update aaa.m

--- aaa.m
+++ aaa.m
@@ -1,81 +1,185 @@
-//
-//  ViewController.m
-//  task
-//
-//  Created by Lucas on 12/2/24.
-//
+#import <UIKit/UIKit.h>
 
-#import "ViewController.h"
+typedef NS_ENUM(NSInteger, WatchAlertResult) {
+    WatchAlertResultYes,
+    WatchAlertResultNo,
+    WatchAlertResultTimeout
+};
 
-@interface ViewController ()
-@property (nonatomic, strong) NSTask *task;
+typedef void (^WatchAlertCallback)(WatchAlertResult result);
+
+@interface WatchAlertView : UIView
+
+- (instancetype)initWithTitle:(NSString *)title
+                      message:(NSString *)message
+                    showNoBtn:(BOOL)showNo
+                     callback:(WatchAlertCallback)callback;
+
+- (void)showInView:(UIView *)parentView timeout:(NSTimeInterval)timeout;
+
 @end
 
-@implementation ViewController
+#import "WatchAlertView.h"
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    [self compressTestFiles];
+@interface WatchAlertView ()
+@property (nonatomic, strong) UIVisualEffectView *blurView;
+@property (nonatomic, strong) UIView *dialogView;
+@property (nonatomic, copy) WatchAlertCallback callback;
+@property (nonatomic, assign) BOOL responded;
+@end
+
+@implementation WatchAlertView
+
+- (instancetype)initWithTitle:(NSString *)title
+                      message:(NSString *)message
+                    showNoBtn:(BOOL)showNo
+                     callback:(WatchAlertCallback)callback {
+    
+    CGRect screenBounds = [UIScreen mainScreen].bounds;
+    self = [super initWithFrame:screenBounds];
+    if (self) {
+        self.callback = callback;
+        self.responded = NO;
+
+        // 模糊背景
+        UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+        self.blurView = [[UIVisualEffectView alloc] initWithEffect:blur];
+        self.blurView.frame = self.bounds;
+        [self addSubview:self.blurView];
+
+        // 弹窗视图
+        CGFloat dialogW = screenBounds.size.width * 0.85;
+        CGFloat dialogH = 150;
+        self.dialogView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, dialogW, dialogH)];
+        self.dialogView.center = self.center;
+        self.dialogView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.85];
+        self.dialogView.layer.cornerRadius = 12;
+        [self addSubview:self.dialogView];
+
+        CGFloat padding = 10;
+
+        // Title
+        UILabel *titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, padding, dialogW - 2*padding, 22)];
+        titleLabel.text = title;
+        titleLabel.textColor = [UIColor whiteColor];
+        titleLabel.textAlignment = NSTextAlignmentCenter;
+        titleLabel.font = [UIFont boldSystemFontOfSize:16];
+        titleLabel.adjustsFontSizeToFitWidth = YES;
+        titleLabel.minimumScaleFactor = 0.5;
+        [self.dialogView addSubview:titleLabel];
+
+        // Message
+        UILabel *messageLabel = [[UILabel alloc] initWithFrame:CGRectMake(padding, 40, dialogW - 2*padding, 40)];
+        messageLabel.text = message;
+        messageLabel.textColor = [UIColor lightGrayColor];
+        messageLabel.textAlignment = NSTextAlignmentCenter;
+        messageLabel.numberOfLines = 2;
+        messageLabel.font = [UIFont systemFontOfSize:14];
+        messageLabel.adjustsFontSizeToFitWidth = YES;
+        messageLabel.minimumScaleFactor = 0.5;
+        [self.dialogView addSubview:messageLabel];
+
+        // Buttons
+        CGFloat buttonY = 95;
+        CGFloat buttonW = 60;
+
+        UIButton *yesBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+        [yesBtn setTitle:@"Yes" forState:UIControlStateNormal];
+        [yesBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        yesBtn.layer.borderColor = [UIColor whiteColor].CGColor;
+        yesBtn.layer.borderWidth = 1;
+        yesBtn.layer.cornerRadius = 5;
+        [yesBtn addTarget:self action:@selector(yesTapped) forControlEvents:UIControlEventTouchUpInside];
+
+        UIButton *noBtn;
+        if (showNo) {
+            yesBtn.frame = CGRectMake(dialogW / 2 - 70, buttonY, buttonW, 30);
+            noBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+            [noBtn setTitle:@"No" forState:UIControlStateNormal];
+            [noBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+            noBtn.layer.borderColor = [UIColor redColor].CGColor;
+            noBtn.layer.borderWidth = 1;
+            noBtn.layer.cornerRadius = 5;
+            noBtn.frame = CGRectMake(dialogW / 2 + 10, buttonY, buttonW, 30);
+            [noBtn addTarget:self action:@selector(noTapped) forControlEvents:UIControlEventTouchUpInside];
+            [self.dialogView addSubview:noBtn];
+        } else {
+            yesBtn.frame = CGRectMake((dialogW - buttonW) / 2, buttonY, buttonW, 30);
+        }
+
+        [self.dialogView addSubview:yesBtn];
+    }
+    return self;
 }
 
-- (void)compressTestFiles {
-    NSString *workingDir = @"/Users/lucas/Desktop/00000";
-    
-    // 创建 shell 任务
-    self.task = [[NSTask alloc] init];
-    self.task.launchPath = @"/bin/sh";
-    self.task.currentDirectoryPath = workingDir;
-    
-    // 创建 shell 命令：使用 -C 选项指定工作目录
-    NSString *command = @"tar -czf test.tar -C /Users/lucas/Desktop/00000 test*.log";
-    self.task.arguments = @[@"-c", command];
-    
-    // 设置输出管道
-    NSPipe *pipe = [NSPipe pipe];
-    self.task.standardError = pipe;
-    self.task.standardOutput = pipe;
-    
-    self.task.terminationHandler = ^(NSTask *task) {
-        NSFileHandle *fileHandle = [pipe fileHandleForReading];
-        NSData *data = [fileHandle readDataToEndOfFile];
-        NSString *output = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (task.terminationStatus != 0) {
-                NSLog(@"压缩失败，退出码: %d", task.terminationStatus);
-                NSLog(@"输出信息: %@", output);
-            } else {
-                NSLog(@"压缩成功完成");
-                
-                // 创建解压任务来测试
-                NSTask *untar = [[NSTask alloc] init];
-                untar.launchPath = @"/usr/bin/tar";
-                untar.arguments = @[@"-xf", @"test.tar"];
-                untar.currentDirectoryPath = workingDir;
-                
-                NSError *untarError;
-                [untar launchAndReturnError:&untarError];
-                if (untarError) {
-                    NSLog(@"解压测试失败: %@", untarError);
-                }
+- (void)showInView:(UIView *)parentView timeout:(NSTimeInterval)timeout {
+    self.alpha = 0;
+    [parentView addSubview:self];
+
+    [UIView animateWithDuration:0.25 animations:^{
+        self.alpha = 1.0;
+    }];
+
+    if (timeout > 0) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(timeout * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            if (!self.responded) {
+                [self performCallback:WatchAlertResultTimeout];
             }
         });
-    };
-    
-    NSError *error;
-    
-    // 启动任务
-    [self.task launchAndReturnError:&error];
-    if (error) {
-        NSLog(@"启动任务失败: %@", error);
     }
 }
 
-- (void)setRepresentedObject:(id)representedObject {
-    [super setRepresentedObject:representedObject];
-
-    // Update the view, if already loaded.
+- (void)yesTapped {
+    if (!self.responded) {
+        [self performCallback:WatchAlertResultYes];
+    }
 }
 
+- (void)noTapped {
+    if (!self.responded) {
+        [self performCallback:WatchAlertResultNo];
+    }
+}
+
+- (void)performCallback:(WatchAlertResult)result {
+    self.responded = YES;
+    __weak typeof(self) weakSelf = self;
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        if (weakSelf.callback) {
+            weakSelf.callback(result);
+        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [UIView animateWithDuration:0.2 animations:^{
+                weakSelf.alpha = 0;
+            } completion:^(BOOL finished) {
+                [weakSelf removeFromSuperview];
+            }];
+        });
+    });
+}
 
 @end
+
+#import "WatchAlertView.h"
+
+...
+
+WatchAlertView *alert = [[WatchAlertView alloc] initWithTitle:@"删除项目"
+                                                      message:@"该操作无法撤销，是否继续？"
+                                                    showNoBtn:YES
+                                                     callback:^(WatchAlertResult result) {
+    switch (result) {
+        case WatchAlertResultYes:
+            NSLog(@"✅ 用户点击了 Yes");
+            break;
+        case WatchAlertResultNo:
+            NSLog(@"❌ 用户点击了 No");
+            break;
+        case WatchAlertResultTimeout:
+            NSLog(@"⌛ 超时无响应");
+            break;
+    }
+}];
+
+[alert showInView:self.view timeout:5];
